4dcc348a75c11e91a3c72f33263ab035645c767e
skeks <azarov990@gmail.com>
2026-07-03 11:56:54 +0000

    fix gitignore

--- include/libmath.hpp
+++ include/libmath.hpp
@@ -24,6 +24,14 @@ namespace libmath
         }
         return c;
     }
+    int factorial( int x)
+    {
+        if(x == 0 || x == 1)
+        {
+            return 1;
+        }
+        return x * factorial(x - 1);
+    }
     int division(int x, int y, int &c)
     {
         if (y == 0)
